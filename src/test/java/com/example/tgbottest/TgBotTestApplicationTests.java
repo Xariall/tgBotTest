a6c3fe630dee052cb7dd81e5fc5a2a18f39447f1
Asanali Esmagambetov <xariell@MacBook-Pro-Asanali.local>
2023-11-16 11:26:29 +0600

    first commit

---
+++ src/test/java/com/example/tgbottest/TgBotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgbottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
